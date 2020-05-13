e444d9c0a74f20e05d9bebee7afda7aacdef5dab
Cristian Franco Bedoya <cfrancobedoya@gmail.com>
2020-05-13 15:44:35 -0500

    Connecting to the remote database.

--- proyectointegrador1/proyectointegrador1/settings.py
+++ proyectointegrador1/proyectointegrador1/settings.py
@@ -75,10 +75,30 @@ WSGI_APPLICATION = 'proyectointegrador1.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'pi1-eafit-db',
+#         'USER': 'root',
+#         'PASSWORD': 'alejandro',
+#         'HOST': 'localhost',
+#         'PORT': '3306'
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'WiZmuTSt3O',
+        'USER': 'WiZmuTSt3O',
+        'PASSWORD': 'WnUr7FDjix',
+        'HOST': 'remotemysql.com',
     }
 }
 

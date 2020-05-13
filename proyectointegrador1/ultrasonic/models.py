a592d7773cffda2bf9a0ed08bf49518f2fed1d85
Cristian Franco Bedoya <cfrancobedoya@gmail.com>
2020-05-13 17:36:56 -0500

    Editing view

--- proyectointegrador1/ultrasonic/models.py
+++ proyectointegrador1/ultrasonic/models.py
@@ -7,5 +7,8 @@ class Ultrasonic(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     type = models.CharField(verbose_name='Tipo', max_length=20)
     value = models.IntegerField(verbose_name='Valor')
+    placeLatitude = models.FloatField(verbose_name='LatitudLugar')
+    placeLength = models.FloatField(verbose_name='PlaceLength')
+    landArea = models.TextField(verbose_name='LandArea')
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
